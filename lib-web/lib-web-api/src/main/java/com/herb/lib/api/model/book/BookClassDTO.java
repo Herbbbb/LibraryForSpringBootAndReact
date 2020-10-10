d185d20b49ffa8dfd448db60b18dbd586bee91c7
Herbbbb <2322915188@qq.com>
2020-10-10 21:10:11 +0800

    feat dev分支与model创建

---
+++ lib-web/lib-web-api/src/main/java/com/herb/lib/api/model/book/BookClassDTO.java
@@ -0,0 +1,21 @@
+package com.herb.lib.api.model.book;
+
+import com.herb.lib.api.model.BaseDTO;
+
+import java.io.Serializable;
+
+/**
+ * 书籍分类实体类
+ * @author 油炸小波
+ * @version 1.0
+ * @date 2020/10/9 20:56
+ */
+public class BookClassDTO extends BaseDTO implements Serializable{
+
+    private static final long serialVersionUID = 6109911930620603904L;
+
+    /**
+     * 分类名称
+     */
+    private String name;
+}
